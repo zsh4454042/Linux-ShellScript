e38cad7c3b8f279b5361be5c61074718786d9a94
zsh4454042 <50446419+zsh4454042@users.noreply.github.com>
2019-07-29 14:38:13 +0800

    Redis和Tomcat的bug修复

--- 应用程序开机自启动（包含Oracle和MongoDB）/RedisAutostart.sh
+++ 应用程序开机自启动（包含Oracle和MongoDB）/RedisAutostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export REDIS_HOME=/usr/local/redis-4.0.6
 echo "----------Redis start---------------"
-redis-server $REDIS_HOME/redis.conf
+/usr/local/bin/redis-server $REDIS_HOME/redis.conf
 echo "----------Redis start successful----"
--- 应用程序开机自启动（包含Oracle和MongoDB）/TomcatAutostart.sh
+++ 应用程序开机自启动（包含Oracle和MongoDB）/TomcatAutostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export JRE_HOME=/opt/jdk1.8.0_152
 export TOMCAT_HOME=/opt/apache-tomcat-8.5.32_chengdu
 echo "----------Tomcat start---------------"
 $TOMCAT_HOME/bin/startup.sh
